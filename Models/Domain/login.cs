8ed37fee03ac5c3aadf7211f4974bc8f1c55b47d
AudelPerez <AudelPerez397@gmail.com>
2024-09-02 15:31:23 -0600

    dddd

--- Models/Domain/login.cs
+++ Models/Domain/login.cs
@@ -10,6 +10,7 @@ namespace pruebaotra.Models.Domain
         public int ad ; 
         public int nd; 
         public int n ; 
-        public decimal ndds;  
+        public decimal ndds; 
+        public int nanadm; 
     }
 }
