7e8bc58ce7230027ffeb68490f2f9979ba7dac0d
AudelPerez <AudelPerez397@gmail.com>
2024-09-02 17:17:30 -0600

    listo

--- obj/Debug/net8.0/pruebaotra.AssemblyInfo.cs
+++ obj/Debug/net8.0/pruebaotra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pruebaotra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84586163f35de9073568ec73e05da22851b6a0b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed0892470156deb70b78d98f0de02407d01981e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("pruebaotra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pruebaotra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
