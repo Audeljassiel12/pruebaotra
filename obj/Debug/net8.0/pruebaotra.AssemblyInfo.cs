6cbbf397472884eab410d10e3dd03d262119c470
AudelPerez <AudelPerez397@gmail.com>
2024-09-02 14:58:20 -0600

    cree modelo pasiente

--- obj/Debug/net8.0/pruebaotra.AssemblyInfo.cs
+++ obj/Debug/net8.0/pruebaotra.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pruebaotra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+198171d4ac7928216a3afde0c9b2c00343bcd6d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("pruebaotra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pruebaotra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
